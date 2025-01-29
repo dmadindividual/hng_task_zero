f2155582e72d5cbf89a44011097cd4532f460f7a
Falade Ayomide <109168338+dmadindividual@users.noreply.github.com>
2025-01-29 13:09:20 +0100

    Update HngController.java

--- src/main/java/topg/hng_task_one/controller/HngController.java
+++ src/main/java/topg/hng_task_one/controller/HngController.java
@@ -18,9 +18,11 @@ public class HngController {
     @GetMapping
     public Map<String, String> getDetails() {
         Map<String, String> response = new HashMap<>();
-        response.put("email", "ayomidefalade@gmail.com"); 
-        response.put("current_datetime", DateTimeFormatter.ISO_INSTANT.format(Instant.now()));
+     
+       
         response.put("github_url", "https://github.com/dmadindividual/hng_task_zero.git");
+         response.put("current_datetime", DateTimeFormatter.ISO_INSTANT.format(Instant.now()));
+           response.put("email", "ayomidefalade@gmail.com"); 
         return response;
     }
 }
