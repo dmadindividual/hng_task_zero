35d55c186bd48506a9f69d45bc48beee5dd39570
dmadindividual <ayomidefalade@gmail.com>
2025-01-29 12:18:08 +0100

    first commit

---
+++ src/main/java/topg/hng_task_one/HngTaskOneApplication.java
@@ -0,0 +1,13 @@
+package topg.hng_task_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HngTaskOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HngTaskOneApplication.class, args);
+	}
+
+}
---
+++ src/main/java/topg/hng_task_one/controller/HngController.java
@@ -0,0 +1,26 @@
+package topg.hng_task_one.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "*")
+public class HngController {
+
+    @GetMapping
+    public Map<String, String> getDetails() {
+        Map<String, String> response = new HashMap<>();
+        response.put("email", "ayomidefalade@gmail.com"); // Replace with your registered email
+        response.put("current_datetime", DateTimeFormatter.ISO_INSTANT.format(Instant.now()));
+        response.put("github_url", "https://github.com/dmadindividual/hng_task_zero.git");
+        return response;
+    }
+}
